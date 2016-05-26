e9006de04a3467ad54af621bc269ff42e3756797
Wataru Watanabe <wataru.watanabe@gmail.com>
2016-05-26 11:23:26 +0700

    initial commit
    Completed step 9 (Security & Methods) of Meteor/Blaze simple-todos
    tutorial

---
+++ imports/ui/task.js
@@ -0,0 +1,16 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+
+import { Tasks } from '../api/tasks.js';
+
+import './task.html';
+
+Template.task.events({
+  'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Meteor.call('tasks.setChecked', this._id, !this.checked);
+  },
+  'click .delete'() {
+    Meteor.remove('tasks.remove', this._id);
+  },
+});
